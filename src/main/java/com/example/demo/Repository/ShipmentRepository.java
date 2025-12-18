65204852c1dbd956949e95217fb85033979e4375
bala-amypo <bala@amypo.in>
2025-12-18 09:28:42 +0000

    User commit

---
+++ src/main/java/com/example/demo/Repository/ShipmentRepository.java
@@ -0,0 +1,16 @@
+package com.example.demo.repository;
+
+
+
+import com.example.demo.entity.Shipment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface ShipmentRepository extends JpaRepository<Shipment, Long> {
+
+}
+
+
+}
---
+++ src/main/java/com/example/demo/Repository/VehicleRepository.java
@@ -0,0 +1,16 @@
+package com.example.demo.repository;
+
+
+
+import com.example.demo.entity.Vehicle;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
+
+    }
+
+    
+}
